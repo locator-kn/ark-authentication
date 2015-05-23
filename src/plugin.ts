cdb73ba42369af39932d8eb0283a722378d3c174
T1m1 <Ruprecht.t@gmx.de>
2015-05-23 13:20:21 +0200

    adjust route

--- src/plugin.ts
+++ src/plugin.ts
@@ -163,7 +163,7 @@ class ArkAuth {
 
         server.route({
             method: ['GET'],
-            path: '/users/password/forgotten/{mail}',
+            path: '/password/forgotten/{mail}',
             config: {
                 auth: false,
                 handler: this.passwordForgotten,
@@ -305,6 +305,24 @@ class ArkAuth {
             }
         })
     }
+
+    passwordForgotten = (request, reply) => {
+        // TODO check if user mail exists
+        // TODO response frontend -> email not exists
+        // TODO password - stärke check?
+        // TODO ? set auth token
+        // TODO ? set timestamp
+        //
+
+
+        // twitter:
+        // /begin_password_reset
+        // - email
+        // - auth. token: 0e67a0e055961639a77740c56fd5a0205a9e32a8
+        // https://twitter.com/account/confirm_email_reset?reset_type=e&user_id=2573733729&token=JTfiACM7R3r3ChgOdPK9fjkYByyB8gwQFwMEBDAA7dI=-1432283924932&confirm_pending_email=0&token_version=0&password_reset_cause=user
+        // /account/reset_passwordd
+        // 0e67a0e055961639a77740c56fd5a0205a9e32a8
+    };
 
     errorInit(error) {
         if (error) {
